4dfe42664dd4e897f6e1fd1cd25c331cd24762cb
Neo <neo.fanetti@me.com>
2024-02-25 21:34:10 +0100

    implement realtime chat fix

--- src/components/ChatSection/ChatSection.tsx
+++ src/components/ChatSection/ChatSection.tsx
@@ -1,10 +1,10 @@
-import { ReactElement } from 'react';
+import { ReactElement, useEffect } from 'react';
 import { ChatHeader } from '../ChatHeader/ChatHeader';
 import { ChatInput } from '../ChatInput/ChatInput';
 import { DisplayMessages } from '../DisplayMessages/DisplayMessages';
 import { useChat } from '../../hooks/useChat';
 import { useMessages } from '../../hooks/useMessages';
-import { socketService } from '../../services/socketService';
+import io from 'socket.io-client';
 
 export const ChatSection: React.FC<{
   className?: string;
@@ -15,21 +15,79 @@ export const ChatSection: React.FC<{
   const { messages, setFilter } = useMessages(userId, groupId);
 
   // TODO: outsource later on in hook!
-  socketService.connect();
-  if (userId) {
-    socketService.joinChat(userId, undefined);
-  }
+  // const socket = io('http://127.0.0.1:3001');
+  // useEffect(() => {
+  //   socket.on('connect', () => {
+  //     console.log('Verbunden mit dem WebSocketGateway-Server');
 
-  if (groupId) {
-    socketService.joinChat(undefined, groupId);
-  }
-  // socketService.joinChat(userId = userId, groupId);
-  // console.log('userId ' + userId, 'groupId ' + groupId);
+  //     // test workflow with same user (neoo.lf), same mesage ()
+
+  //     if (userId === 'cbab1e58-bea2-43bc-a803-46e7c63967df') {
+  //       socket.emit('join', 'wexxer', undefined);
+
+  //       socket.emit('message', {
+  //         fromUserId: '41ff2b88-0b76-43d9-9d96-39a0c2aa1a2e',
+  //         content: 'Die ersti nachricht usem FRONTEND!!!',
+  //         toUserId: 'cbab1e58-bea2-43bc-a803-46e7c63967df',
+  //       });
+  //     }
+  //   });
+
+  //   if (userId === '41ff2b88-0b76-43d9-9d96-39a0c2aa1a2e') {
+  //     socket.emit('join', 'wexxer', undefined);
+  //   }
+
+  //   socket.on('message', (message) => {
+  //     console.log('Nachricht vom Server erhalten:', message);
+  //   });
+
+  //   return () => {
+  //     socket.disconnect(); // Verbindung beim Komponentenabbau trennen
+  //   };
+  // }, []);
+
+  // FIXME: new try
+
+  const socket = io('http://127.0.0.1:3001', {
+    query: {
+      userId:
+        userId === 'cbab1e58-bea2-43bc-a803-46e7c63967df'
+          ? '41ff2b88-0b76-43d9-9d96-39a0c2aa1a2e'
+          : 'cbab1e58-bea2-43bc-a803-46e7c63967df',
+    },
+    transports: ['websocket'],
+  });
+
+  socket.on('connect_error', (err) => {
+    console.log(`connect_error due to ${err.message}`);
+  });
+
+  useEffect(() => {
+    // Event-Handler zum Empfangen von Nachrichten vom Server
+    socket.on('privateMessage', (message) => {
+      // setMessages(prevMessages => [...prevMessages, message]);
+      console.log('INCOMMING MESSAGE', message);
+    });
+
+    // Cleanup-Funktion zum Trennen des Sockets bei unmounting
+    return () => {
+      socket.disconnect();
+    };
+  });
+
+  const sendMessage = (inputMessage: string) => {
+    const receiverId = 'cbab1e58-bea2-43bc-a803-46e7c63967df'; // Empfänger-ID hier einfügen
+    socket.emit('privateMessage', {
+      toUserId: receiverId,
+      content: inputMessage,
+    });
+    // setInputMessage('');
+  };
 
   const handleClick = () => {
     console.log('message sent');
-    // socketService.sendMessage()
     // TODO: call function that sends message
+    sendMessage('DIE ERSTI NACHRICHT VOM BUTTON UEBER PRISMA!!!!');
   };
 
   return (
--- src/services/socketService.ts
+++ src/services/socketService.ts
@@ -1,52 +1,52 @@
-import { io, Socket } from "socket.io-client";
-import { ClientToServerEvents, ServerToClientEvents } from "../types/event.type";
-import { CreateMessageType } from "../types/create-message-type";
+// import { io, Socket } from "socket.io-client";
+// import { ClientToServerEvents, ServerToClientEvents } from "../types/event.type";
+// import { CreateMessageType } from "../types/create-message-type";
 
-class SocketService {
-  private readonly socket: Socket<ServerToClientEvents, ClientToServerEvents> =
-    io(import.meta.env.VITE_BASE_WS_URL, {
-      autoConnect: false,
-    });
+// class SocketService {
+//   private readonly socket: Socket<ServerToClientEvents, ClientToServerEvents> =
+//     io(import.meta.env.VITE_BASE_WS_URL, {
+//       autoConnect: false,
+//     });
 
-  // connectWithAuthToken(token: string) {
-  //   this.socket.auth = { token };
-  //   this.socket.connect();
-  // }
-  connect() {
-    this.socket.connect();
-  }
+//   // connectWithAuthToken(token: string) {
+//   //   this.socket.auth = { token };
+//   //   this.socket.connect();
+//   // }
+//   connect() {
+//     this.socket.connect();
+//   }
 
-  disconnect() {
-    this.socket.disconnect();
-  }
+//   disconnect() {
+//     this.socket.disconnect();
+//   }
 
-  sendMessage(data: CreateMessageType) {
-    this.socket.emit("message", data);
-  }
+//   sendMessage(data: CreateMessageType) {
+//     this.socket.emit("message", data);
+//   }
 
-  // notifyTyping(roomId: number) {
-  //   this.socket.emit("isTyping", roomId);
-  // }
+//   // notifyTyping(roomId: number) {
+//   //   this.socket.emit("isTyping", roomId);
+//   // }
 
-  subscribeToMessages(messageHandler: ServerToClientEvents["message"]) {
-    console.log('user listener to messages');
-    this.socket.on("message", messageHandler);
-  }
+//   subscribeToMessages(messageHandler: ServerToClientEvents["message"]) {
+//     console.log('user listener to messages');
+//     this.socket.on("message", messageHandler);
+//   }
 
-  // subscribeToTypingNotifications(
-  //   typingNotificationsHandler: ServerToClientEvents["isTyping"]
-  // ) {
-  //   this.socket.on("isTyping", typingNotificationsHandler);
-  // }
+//   // subscribeToTypingNotifications(
+//   //   typingNotificationsHandler: ServerToClientEvents["isTyping"]
+//   // ) {
+//   //   this.socket.on("isTyping", typingNotificationsHandler);
+//   // }
 
-  joinChat(userId?: string, groupId?: string) {
-    console.log(userId, groupId);
-    this.socket.emit("join", userId, groupId);
-  }
+//   joinChat(userId?: string, groupId?: string) {
+//     console.log(userId, groupId);
+//     this.socket.emit("join", userId, groupId);
+//   }
 
-  leaveChat(userId?: string, groupId?: string) {
-    this.socket.emit("leave", userId, groupId);
-  }
-}
+//   leaveChat(userId?: string, groupId?: string) {
+//     this.socket.emit("leave", userId, groupId);
+//   }
+// }
 
-export const socketService = new SocketService();
+// export const socketService = new SocketService();
